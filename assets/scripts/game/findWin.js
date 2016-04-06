77ab6873f18a55b93887494748e71e380184ba8f
louarnos <louarnos@gmail.com>
2016-04-06 14:57:55 -0400

    Game Win Conditions Complete
    Need to still notify user

--- assets/scripts/game/findWin.js
+++ assets/scripts/game/findWin.js
@@ -6,21 +6,30 @@ const findWinOrTie = function () {
   let win = false;
   let tie = false;
   let winner;
-  if (((typeof game.game.game.cells[0]) !== 'undefined') && game.game.game.cells[0] === game.game.game.cells[1] && game.game.game.cells[1] === game.game.game.cells[2]) {
+  if ((game.game.game.cells[0] !== '') && game.game.game.cells[0] === game.game.game.cells[1] && game.game.game.cells[1] === game.game.game.cells[2]) {
     win = true;
     winner = game.game.game.cells[0];
-  }else if (((typeof game.game.game.cells[3]) !== 'undefined') && game.game.game.cells[3] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[5]) {
+  }else if ((game.game.game.cells[3] !== '') && game.game.game.cells[3] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[5]) {
     win = true;
     winner = game.game.game.cells[3];
-  }else if (((typeof game.game.game.cells[6]) !== 'undefined') && game.game.game.cells[6] === game.game.game.cells[7] && game.game.game.cells[7] === game.game.game.cells[8]) {
+  }else if ((game.game.game.cells[6] !== '') && game.game.game.cells[6] === game.game.game.cells[7] && game.game.game.cells[7] === game.game.game.cells[8]) {
     win = true;
     winner = game.game.game.cells[6];
-  }else if (((typeof game.game.game.cells[0]) !== 'undefined') && game.game.game.cells[0] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[8]) {
+  }else if ((game.game.game.cells[0] !== '') && game.game.game.cells[0] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[8]) {
     win = true;
     winner = game.game.game.cells[0];
-  }else if (((typeof game.game.game.cells[2]) !== 'undefined') && game.game.game.cells[2] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[6]) {
+  }else if ((game.game.game.cells[2] !== '') && game.game.game.cells[2] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[6]) {
     win = true;
     winner = game.game.game.cells[2];
+  }else if ((game.game.game.cells[2] !== '') && game.game.game.cells[2] === game.game.game.cells[5] && game.game.game.cells[5] === game.game.game.cells[8]) {
+    win = true;
+    winner = game.game.game.cells[2];
+  }else if ((game.game.game.cells[1] !== '') && game.game.game.cells[1] === game.game.game.cells[4] && game.game.game.cells[4] === game.game.game.cells[7]) {
+    win = true;
+    winner = game.game.game.cells[1];
+  }else if ((game.game.game.cells[0] !== '') && game.game.game.cells[0] === game.game.game.cells[3] && game.game.game.cells[3] === game.game.game.cells[6]) {
+    win = true;
+    winner = game.game.game.cells[0];
   }else if (game.moveCount >= 9) {
     tie = true;
   }
